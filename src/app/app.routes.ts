e8f31dfd053c564698fa80372bd6010960e14816
valerioprds <v.paradiso89@gmail.com>
2024-04-18 13:32:25 +0200

    added components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ClassesComponent } from './components/classes/classes.component';
+import { ClientFormComponent } from './components/client-form/client-form.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { RatesComponent } from './components/rates/rates.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'clases', component: ClassesComponent },
+  { path: 'client-form', component: ClientFormComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: 'tarifas', component: RatesComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
---
+++ src/app/components/rates/rates.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rates',
+  standalone: true,
+  imports: [],
+  templateUrl: './rates.component.html',
+  styleUrl: './rates.component.scss'
+})
+export class RatesComponent {
+
+}
