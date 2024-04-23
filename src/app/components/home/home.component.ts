4c65d407d56b94411b5aa350b8281063520f4ec2
valerioprds <v.paradiso89@gmail.com>
2024-04-23 13:02:39 +0200

    added rates to rate component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
+import { RatesComponent } from "../rates/rates.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, FooterComponent]
+    imports: [HeaderComponent, FooterComponent, RatesComponent]
 })
 export class HomeComponent {
 
--- src/app/components/rates/rates.component.ts
+++ src/app/components/rates/rates.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
-  selector: 'app-rates',
-  standalone: true,
-  imports: [],
-  templateUrl: './rates.component.html',
-  styleUrl: './rates.component.scss'
+    selector: 'app-rates',
+    standalone: true,
+    templateUrl: './rates.component.html',
+    styleUrl: './rates.component.scss',
+    imports: [HeaderComponent, FooterComponent]
 })
 export class RatesComponent {
 
