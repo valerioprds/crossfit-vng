5d6698402655a8953b11d3aff909c46254bac4ee
valerioprds <v.paradiso89@gmail.com>
2024-04-23 16:37:50 +0200

    added swiper

--- src/app/components/classes/classes.component.ts
+++ src/app/components/classes/classes.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { SwiperModule } from 'swiper/angular';
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  SwiperOptions,
+  Swiper,
+} from 'swiper';
+import { CommonModule } from '@angular/common';
+
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 @Component({
   selector: 'app-classes',
   standalone: true,
-  imports: [],
+  imports: [SwiperModule, CommonModule],
   templateUrl: './classes.component.html',
-  styleUrl: './classes.component.scss'
+  styleUrl: './classes.component.scss',
 })
 export class ClassesComponent {
+  config: SwiperOptions = {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 50,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
 
+  slides = [
+    {
+      image: '/assets/images/CF-VNG LOGO versiones-01.jpg',
+    },
+    {
+      image: '/assets/images/felipe.jpg',
+    },
+    {
+      image: '/assets/images/WOD2.jpg',
+    },
+
+  ];
 }
