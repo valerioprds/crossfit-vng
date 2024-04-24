2d1d8993f3ec900d20cde63cff6ba9410d8d0227
valerioprds <v.paradiso89@gmail.com>
2024-04-24 17:32:53 +0200

    changed background color for the entire app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,  Renderer2, ElementRef, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,6 +9,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'crossfit';
+export class AppComponent implements OnInit {
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit() {
+    this.renderer.setStyle(document.body, 'background-color', '#212529');
+  }
 }
