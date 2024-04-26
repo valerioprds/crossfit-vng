f58ad28745d88fb98b99389d0eddab697899be78
valerioprds <v.paradiso89@gmail.com>
2024-04-26 16:07:45 +0200

    added new timetable component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,17 +3,19 @@ import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RatesComponent } from '../rates/rates.component';
 import { ClassesSliderComponent } from '../classes/classes-slider.component';
+import { TimetableComponent } from "../timetable/timetable.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  imports: [
-    HeaderComponent,
-    FooterComponent,
-    RatesComponent,
-    ClassesSliderComponent,
-  ],
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss',
+    imports: [
+        HeaderComponent,
+        FooterComponent,
+        RatesComponent,
+        ClassesSliderComponent,
+        TimetableComponent
+    ]
 })
 export class HomeComponent {}
