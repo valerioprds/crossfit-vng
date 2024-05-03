edafb79e0d291844faf05a0bc39d7831673f8909
valerioprds <v.paradiso89@gmail.com>
2024-05-03 18:42:37 +0200

    put all information on dummy JSON file

--- src/app/components/rates/rates.component.ts
+++ src/app/components/rates/rates.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
-import { FooterComponent } from "../footer/footer.component";
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-rates',
-    standalone: true,
-    templateUrl: './rates.component.html',
-    styleUrl: './rates.component.scss',
-    imports: [HeaderComponent, FooterComponent]
+  selector: 'app-rates',
+  standalone: true,
+  templateUrl: './rates.component.html',
+  styleUrl: './rates.component.scss',
+  imports: [HeaderComponent, FooterComponent, HttpClientModule, CommonModule],
 })
-export class RatesComponent {
+export class RatesComponent implements OnInit {
+  rates: any[] = [];
 
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.loadRates();
+  }
+
+  loadRates() {
+    this.http.get<any[]>('/assets/rates.json').subscribe((data) => {
+      this.rates = data;
+    });
+  }
 }
