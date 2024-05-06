221711be0b1f529db4025a601b38cf4c2cd127b4
valerioprds <v.paradiso89@gmail.com>
2024-05-06 12:05:10 +0200

    now modal popup works and can send message to whatsapp

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-popup',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './popup.component.html',
-  styleUrl: './popup.component.scss'
+  styleUrl: './popup.component.scss',
 })
 export class PopupComponent {
+  contactForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      firstName: [''],
+      lastName: [''],
+      email: [''],
+      phone: [''],
+      dropdown: [''],
+      checkbox1: [false],
+      checkbox2: [false],
+    });
+  }
+
+  sendToWhatsApp() {
+    const {
+      firstName,
+      lastName,
+      email,
+      phone,
+      dropdown,
+      checkbox1,
+      checkbox2,
+    } = this.contactForm.value;
+    const message = `Hello, my name is ${firstName} ${lastName}. My email is ${email}, phone ${phone}. Selected ${dropdown}, Checkbox1: ${
+      checkbox1 ? 'Yes' : 'No'
+    }, Checkbox2: ${checkbox2 ? 'Yes' : 'No'}`;
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=+34634671046&text=${encodeURIComponent(
+      message
+    )}`;
+    window.open(whatsappUrl, '_blank');
+  }
 }
