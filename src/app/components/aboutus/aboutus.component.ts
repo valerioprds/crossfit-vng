262f919f2f9cd78331932276cc8ee085e2b714bc
valerioprds <v.paradiso89@gmail.com>
2024-05-07 11:54:55 +0200

    about us work in progress

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/header/header.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
-  selector: 'app-aboutus',
-  standalone: true,
-  imports: [],
-  templateUrl: './aboutus.component.html',
-  styleUrl: './aboutus.component.scss'
+    selector: 'app-aboutus',
+    standalone: true,
+    templateUrl: './aboutus.component.html',
+    styleUrl: './aboutus.component.scss',
+    imports: [HeaderComponent, FooterComponent]
 })
 export class AboutusComponent {
 
